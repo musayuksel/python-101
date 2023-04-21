88bfc516c6e39f7fb5c719e585776fd2b16526f6
Musa <79163385+musayuksel@users.noreply.github.com>
2023-04-21 13:25:25 +0100

    fix function for negative numbers

--- Basics/ifStatetments.py
+++ Basics/ifStatetments.py
@@ -1,11 +1,11 @@
 my_number = input("Enter a number: ")
-if my_number.isdigit():
+try:
     my_number = int(my_number)
     if my_number % 2 == 0:
         print("Even")
     else:
         print("Odd")
 
-else:
+except ValueError:
     print(f''' {my_number} is not a number.
     Please enter a number''')
