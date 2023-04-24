520d168f588c06fb942bf7330a0763dd4fddf7c9
Musa <79163385+musayuksel@users.noreply.github.com>
2023-04-24 06:56:38 +0100

    Break and Continue

--- Basics/main.py
+++ Basics/main.py
@@ -18,3 +18,15 @@ i = 0
 while i < age:
     print(str(i) + " years old!")
     i += 1
+
+
+# BREAK and CONTINUE
+# We can use them with for and while loops
+number = 0
+
+while (number < 10):
+    number += 1
+    if number % 2 == 0:  # if number is even
+        continue
+        # break # break the loop
+    print(number)
