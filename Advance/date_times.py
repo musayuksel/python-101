e1eb2e38eb133cba02600b4b647971bb42fc3e92
Musa <79163385+musayuksel@users.noreply.github.com>
2023-04-24 09:41:03 +0100

    Date times

---
+++ Advance/date_times.py
@@ -0,0 +1,9 @@
+import datetime  # OR
+# from datetime import date
+
+# Create a datetime object
+uk_entry_time = datetime.datetime(2023, 2, 4, 12, 15, 58)
+print(f"uk_entry_time:  {uk_entry_time}")
+
+print(f"NOW: {datetime.datetime.now()}")
+print(f"Today: {datetime.date.today()}")
