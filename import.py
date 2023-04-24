70dad62bfe82ab4bffbc2207775e1d70870ab35d
Musa <79163385+musayuksel@users.noreply.github.com>
2023-04-24 08:02:33 +0100

    Different imports

--- import.py
+++ import.py
@@ -1,9 +1,14 @@
 # We can use the import keyword to import a module
-from math import pi
+# WE CAN USE DIFFERENT IMPORT SYNTAXES
+# ALSO WE can import from a specific file
+from math import pi  # OR
 import math
+from Basics.forLoop_map_filter import myFunc  # OR
+import Basics.forLoop_map_filter as forLoop_map_filter
+forLoop_map_filter.myFunc("apple")
+
 
 print(math.pi)
 
-# WE CAN USE DIFFERENT IMPORT SYNTAXES
 # import math as m
 print(pi)
