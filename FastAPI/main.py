b02fc7649ef9591604c94e1bd13c11ed922f6497
Musa <79163385+musayuksel@users.noreply.github.com>
2023-04-25 09:01:35 +0100

    Add request body

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,8 +1,17 @@
 # uvicorn main:app --reload  # --reload: auto reload when code changes
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel  # for request body
 
 app = FastAPI()
+
+# Request body
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool = None
 
 
 @app.get("/")
@@ -13,3 +22,8 @@ async def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, query: Union[str, None] = None):
     return {"item_id": item_id, "query": query}
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
