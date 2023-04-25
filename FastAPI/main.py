0d5f33d8221fa1e8a7abb1e2559050ce6c71defd
Musa <79163385+musayuksel@users.noreply.github.com>
2023-04-25 12:29:53 +0100

    Add comments

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,8 +1,12 @@
 # uvicorn main:app --reload  # --reload: auto reload when code changes
 # from typing import Union
+# FastAPI is a Python class that provides all the functionality for your API.
 from fastapi import FastAPI
 from pydantic import BaseModel  # for request body
 
+# The app variable will be an "instance" of the class FastAPI.
+# This will be the main point of interaction to create all your API.
+# This app is the same one referred by uvicorn in the command:uvicorn main:app --reload
 app = FastAPI()
 
 # Request body
@@ -18,9 +22,12 @@ class Item(BaseModel):
 items: list[Item] = []
 
 
-@app.get("/")
+@app.get("/")  # in OpenAPI, each of the HTTP methods is called an "operation".
+# Define a path operation decorator
 async def read_root():
     return {"message": "Hello World!"}
+
+# SIMILAR TO NODEJS ROUTES, THE ORDER MATTERS FOR THE PATHS
 
 
 @app.get("/items/")
@@ -29,10 +36,14 @@ async def read_items():
     return {"items": items}
 
 
+# http://127.0.0.1:8000/items/5?query=Musa&isGift=false  # # isGift is optional and might be False | false | 0 | 1 | True | true |on | off| yes | no
+# # query is optional
 @app.get("/items/{item_id}")
+# The value of the path parameter item_id will be passed to your function as the argument item_id
 # async def read_item(item_id: int, query: Union[str, None] = None):
-async def read_item(item_id: int, query: str | None = None):
-    return {"item_id": item_id, "query": query}
+async def read_item(item_id: int, query: str | None = None, isGift: bool = False):
+    # with that type declaration, FastAPI gives you automatic request "parsing".
+    return {"item_id": item_id, "query": query, "isGift": isGift}
 
 
 @app.put("/items/{item_id}")
