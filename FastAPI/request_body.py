8c01e7c74ac978d9a6c5737f7444d9703dcc9006
Musa <79163385+musayuksel@users.noreply.github.com>
2023-04-26 12:49:36 +0100

    Add validation using Annotated

--- FastAPI/request_body.py
+++ FastAPI/request_body.py
@@ -1,6 +1,6 @@
 # A request body is data sent by the client to your API. A response body is the data your API sends to the client.
-
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Query
 from pydantic import BaseModel  # for request body
 
 
@@ -41,3 +41,18 @@ async def create_item(item_id: int, item: Item, optional: str | None = None):
     if optional:
         result.update({"optional": optional})
     return result
+
+
+@app.get("/items/")
+# Query parameters with validation
+# Using Annotated is recommended instead of the default value in function parameters
+async def read_items(
+    # query parameters are optional and default to N/A
+    # if you provide a value, it must be between 3 and 50 characters
+    extra_info: Annotated[str, Query(
+        min_length=3, max_length=50)] = "N/A"
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if extra_info:
+        results.update({"extra_info": extra_info})
+    return results
