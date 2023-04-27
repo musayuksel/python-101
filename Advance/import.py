459c68460e6331dd410d79c5623418f68d4feddc
Musa <79163385+musayuksel@users.noreply.github.com>
2023-04-27 15:46:58 +0100

    Move file to the different folder

--- Advance/import.py
+++ Advance/import.py
@@ -3,7 +3,7 @@
 # ALSO WE can import from a specific file
 from math import pi  # OR
 import math
-from Basics.forLoop_map_filter import myFunc  # OR
+from ..Basics.forLoop_map_filter import myFunc  # OR
 import Basics.forLoop_map_filter as forLoop_map_filter
 forLoop_map_filter.myFunc("apple")
 
