5ecc575b52bf463e4a6c08c2187a3e628fec5915
Musa <79163385+musayuksel@users.noreply.github.com>
2023-04-28 13:22:18 +0100

    Add post request endpoint for post

--- PROJECT/Backend/api.py
+++ PROJECT/Backend/api.py
@@ -1,12 +1,17 @@
-from fastapi import FastAPI
-# from pydantic import BaseModel  # for request body
+from fastapi import FastAPI, status
+from pydantic import BaseModel  # for request body
 
 from fastapi.middleware.cors import CORSMiddleware
 from questions import current_quiz
 from user_scores import user_score_list
+
+
+class UserScores(BaseModel):
+    name: str
+    score: int
+
+
 app = FastAPI()
-
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins="*",
@@ -26,3 +31,12 @@ async def user_scores():
     sorted_user_scores = sorted(
         user_score_list, key=lambda user_score:  user_score['score'], reverse=True)
     return {"user_scores": sorted_user_scores}
+
+
+@app.post("/api/user_scores", status_code=status.HTTP_201_CREATED)
+async def user_scores(user_scores: UserScores):
+    user_score = user_scores.dict()
+    next_id = len(user_score_list) + 1
+    user_score.update({"id": next_id})
+    user_score_list.append(user_scores.dict())
+    return {"message": "User score created successfully"}
