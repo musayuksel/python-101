5ec1caf34439cdfb31da4c24cf547a9e73a9c3bc
Musa <79163385+musayuksel@users.noreply.github.com>
2023-04-28 13:47:21 +0100

    Add basic dashboard style

--- PROJECT/Frontend/src/components/Dashboard.jsx
+++ PROJECT/Frontend/src/components/Dashboard.jsx
@@ -3,6 +3,7 @@ import { baseAPILink } from "../App";
 
 export default function Dashboard() {
   const [currentQuizScores, setCurrentQuizScores] = useState([]);
+
   useEffect(() => {
     async function getScores() {
       const response = await fetch(`${baseAPILink}/user_scores`);
@@ -13,11 +14,15 @@ export default function Dashboard() {
   }, []);
 
   return (
-    <ul>
+    <ul className="dashboard-container">
+      <li style={{ fontWeight: "bold" }}>
+        <p>NAME</p>
+        <p>SCORE</p>
+      </li>
       {currentQuizScores.map((user) => (
-        <li key={user.id}>
-          <p>Name: {user.name}</p>
-          <p>Score: {user.score}</p>
+        <li key={`${user.name}-${user.score}`}>
+          <p>{user.name}</p>
+          <p>{user.score}</p>
         </li>
       ))}
     </ul>
