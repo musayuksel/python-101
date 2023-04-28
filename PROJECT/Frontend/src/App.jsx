89a254bd1a3f045ec46782539b153ba49f28884b
Musa <79163385+musayuksel@users.noreply.github.com>
2023-04-28 09:51:35 +0100

    Add header

--- PROJECT/Frontend/src/App.jsx
+++ PROJECT/Frontend/src/App.jsx
@@ -2,19 +2,24 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [questions, setQuestions] = useState([]);
+  const [currentQuiz, setCurrentQuiz] = useState({});
 
   useEffect(() => {
     async function getData() {
-      const response = await fetch("http://127.0.0.1:8000/api/questions");
+      const response = await fetch("http://127.0.0.1:8000/api/current_quiz");
       const data = await response.json();
-      console.log({ data });
-      setQuestions(data);
+      console.log(data);
+      setCurrentQuiz(data.current_quiz);
     }
     getData();
   }, []);
 
-  return <>Hello world!</>;
+  return (
+    <>
+      <h1>Subject: {currentQuiz?.title}</h1>
+      <h2>Total question: {currentQuiz.questions?.length}</h2>
+    </>
+  );
 }
 
 export default App;
