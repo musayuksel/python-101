27d0387ae76ed313e738d765cbf0885d65531ad7
Musa <79163385+musayuksel@users.noreply.github.com>
2023-04-28 12:24:25 +0100

    Add mock user scores

---
+++ PROJECT/Backend/user_scores.py
@@ -0,0 +1,37 @@
+user_score_list = [
+    {
+        "id": 1,
+        "name": "John Mock1",
+        "score": 0
+    },
+    {
+        "id": 2,
+        "name": "John Mock2",
+        "score": 6
+    },
+    {
+        "id": 3,
+        "name": "John Mock3",
+        "score": 5
+    },
+    {
+        "id": 4,
+        "name": "John Mock4",
+        "score": 10
+    },
+    {
+        "id": 5,
+        "name": "John Mock5",
+        "score": 4
+    },
+    {
+        "id": 6,
+        "name": "John Mock6",
+        "score": 3
+    },
+    {
+        "id": 7,
+        "name": "John Mock7",
+        "score": 2
+    }
+]
