35ce8a1d1ea2fc57961c0f62484abb1cb0be4422
Musa <79163385+musayuksel@users.noreply.github.com>
2023-05-02 16:11:31 +0100

    Add run command

--- PostgreSQL_APP/database.py
+++ PostgreSQL_APP/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/postgres"
-
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/quiz_db"
+#  TO RUN THE APP: uvicorn PostgreSQL_APP.main:app --reload
 # Create the SQLAlchemy engine
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
