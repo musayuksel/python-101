d76831e81f34a03b3981a9959bf7e9d8547cb7de
Musa <79163385+musayuksel@users.noreply.github.com>
2023-05-02 07:49:16 +0100

    Fix allow origins

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class UserScores(BaseModel):
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="*",
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
