2ecc33f8f04cd67e7241345269d94f9788edb866
Musa <79163385+musayuksel@users.noreply.github.com>
2023-05-03 13:02:26 +0100

    Add some modules and uses

--- LinkedIn/main.py
+++ LinkedIn/main.py
@@ -1,4 +1,9 @@
 # Define and call your own functions
+import random
+import math
+import calendar
+
+
 def print_hi(name: str) -> None:
     print(f'Hi, {name}')
 
@@ -11,3 +16,17 @@ def square(number: int) -> int:
 
 def multiply(number1: int, number2: int) -> int:
     return number1 * number2
+
+
+print(calendar.month(2023, 5))  # print the calendar of May 2023
+
+print(math.sqrt(4))  # print the square root of 4
+
+# print a random number between 1 and 10 (inclusive)
+print(random.randint(1, 10))
+
+numbers = [12, 23, 45, 67, 65, 43]
+print(random.choice(numbers))  # print a random number from the tuple
+print(random.choice('computer'))  # print a random letter from the string
+random.shuffle(numbers)  # shuffle the list
+print(numbers)
