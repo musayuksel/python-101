3f3b2283afcfa07c3d1669a7788942f9ab890015
Musa <79163385+musayuksel@users.noreply.github.com>
2023-05-03 13:35:59 +0100

    Add lists and sort

---
+++ LinkedIn/lists.py
@@ -0,0 +1,40 @@
+# Arrays in Python are called list
+print(type([]))  # <class 'list'>
+print(type((2, 3)))  # <class 'tuple'> tuple is immutable
+
+
+# 1. Create a list of 5 fruits objects
+# Sort the list in alphabetical order
+# Reverse the list
+# Sort the list with weight
+
+class Fruit:
+    def __init__(self, name: str, weight: int):
+        self.name = name
+        self.weight = weight
+
+    def __repr__(self):  # official string representation of an object
+        return f'Fruit({self.name}, {self.weight})'
+
+    def __str__(self):  # human-readable, or informal, string representation of an object
+        return f'Fruit name is :{self.name}, and the weight is: {self.weight}'
+
+
+fruits = [
+    Fruit('apple', 10),
+    Fruit('banana', 5),
+    Fruit('cherry', 20),
+    Fruit('pear', 15),
+    Fruit('orange', 25)
+]
+
+# fruits.sort(key=lambda fruit: fruit.name) #inplace
+# not inplace and return a new list
+sorted_with_name = sorted(fruits, key=lambda fruit: fruit.name, reverse=True)
+# reverse=True is optional and it is False by default (ascending)
+print(sorted_with_name)
+
+sorted_with_weight = sorted(fruits, key=lambda fruit: fruit.weight)
+print(sorted_with_weight)
+print(str(fruits[0]))  # Fruit name is :apple, and the weight is: 10
+print(repr(fruits[0]))  # Fruit(apple, 10)
