ac81698808b1158542e895e64e550e94c92d8974
Musa <79163385+musayuksel@users.noreply.github.com>
2023-05-04 09:06:59 +0100

    Add exercise 2

--- LinkedIn/Exercises/python_fundamentals.py
+++ LinkedIn/Exercises/python_fundamentals.py
@@ -1,3 +1,4 @@
+######### EXERCISE 1 #########
 # 1) Create a function that takes a dictionary of ANDi names and returns a list of their names in alphabetical order.
 example_input = {'ANDi1': 'John', 'ANDi2': 'Bob', 'ANDi3': 'Alice'}
 # example output: ['Alice', 'Bob', 'John']
@@ -21,3 +22,27 @@ def sort_andis_by_name(andis):
 sort_dict_names = sort_andis_by_name(example_input_andis)
 # [{'name': 'Alice'}, {'name': 'Bob'}, {'name': 'John'}]
 print(sort_dict_names)
+
+######### EXERCISE 2 #########
+# 2) I'm trying to watch some LinkedIn Learning videos to complete my Python Learning Pathway but I keep getting distracted by meme compilations, music recommendations, pets and more on Slack.
+# Your job is to help me create a function that takes a string and checks to see if it contains the following words or phrases: "Dog", "pet", "music", "Funny meme", "Listen to this"
+# If it does, return "NO!" Otherwise, return "Safe watching!"
+
+
+example_input = "I'm trying to watch a video on Python but I keep getting distracted by a dog"
+example_output = "NO!"
+
+example_input2 = "I'm trying to watch a video on Python"
+example_output2 = "Safe watching!"
+
+
+def is_destructive(string: str) -> str:
+    words = ["Dog", "pet", "music", "funny meme", "listen to this"]
+    for word in words:
+        if word.lower() in string.lower():
+            return "NO!"
+    return "Safe watching!"
+
+
+print(is_destructive(example_input))  # NO!
+print(is_destructive(example_input2))  # Safe watching!
