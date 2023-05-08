8dd5e23ce7bf6f47c8e271271155850ebe247ff0
Musa <79163385+musayuksel@users.noreply.github.com>
2023-05-08 21:50:47 +0100

    TUPLES

--- LinkedIn/tuples.py
+++ LinkedIn/tuples.py
@@ -11,3 +11,25 @@ print(myList)
 
 
 # TUPLES
+myTuple = ('a', 'b', 'c', 'd', 'e')
+print(myTuple[0])  # a # we can access the tuple by index
+print(myTuple[1:3])  # ('b', 'c') # we can access a range of items
+# myTuple[0]='z' # TypeError: 'tuple' object does not support item assignment
+
+# Wy we use tuples?
+# 1. Tuples are faster than lists
+# 2. Tuples are immutable
+# 3. Tuples are used to return multiple values from a function
+
+
+def get_person():
+    name = "Harry"
+    age = 30
+    country = "UK"
+    return name, age, country  # return a tuple
+
+
+harry = get_person()
+print(harry)  # ('Harry', 30, 'UK')
+print(type(harry))  # <class 'tuple'>
+# 4. Tuples are used to format strings
