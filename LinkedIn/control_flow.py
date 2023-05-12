83ed446febafaa269d199bbab5d3127bbd53538e
Musa <79163385+musayuksel@users.noreply.github.com>
2023-05-12 09:11:53 +0100

    Add basic if else flow

---
+++ LinkedIn/control_flow.py
@@ -0,0 +1,10 @@
+# Fizz buzz
+for num in range (1,100):
+    output = str(num)
+    if num % 3 == 0:
+        output += "Fizz"
+    if num % 5 == 0:
+        output += "Buzz"
+    
+    # print(output)
+
