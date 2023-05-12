300d467d986fac22db2443acd4e304d4e959b570
Musa <79163385+musayuksel@users.noreply.github.com>
2023-05-12 13:40:06 +0100

    Add post  req test

--- PROJECT/Backend/test_main.py
+++ PROJECT/Backend/test_main.py
@@ -15,4 +15,15 @@ def test_get_current_quiz():
     assert response.status_code == 200
 
     assert response.json()["current_quiz"]["questions"][0]["question"] == "What does the acronym 'UX' stand for?"
-    
+
+def test_user_scores():
+    response = client.get("/api/user_scores")
+    assert response.status_code == 200
+
+    assert response.json()["user_scores"][0]["name"] == "John Mock4"
+
+def test_user_scores_post():
+    response = client.post("/api/user_scores", json={"name": "John Mock4", "score": 4})
+    assert response.status_code == 201
+
+    assert response.json()["message"] == "User score created successfully"
