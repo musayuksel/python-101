eeb06e6480cabe47b6c7842816c7dd428f98f7a5
Musa <79163385+musayuksel@users.noreply.github.com>
2023-05-12 14:06:32 +0100

    Add args and kwargs

---
+++ LinkedIn/args_kwargs.py
@@ -0,0 +1,11 @@
+def sum_all(*args, **kwargs):
+    print(args) # tuple
+    print(kwargs) # dict
+    print( "SUM :", sum(args))
+    # if there is a key called "name" in kwargs, print it
+    if "name" in kwargs:
+        print("NAME: ", kwargs["name"])
+
+sum_all(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15, name="John", age=30, city="New York")
+# (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
+# {'name': 'John', 'age': 30, 'city': 'New York'}
