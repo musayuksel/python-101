ae2d255d2f600831ccc97e39b5293b1effef5b92
Musa <79163385+musayuksel@users.noreply.github.com>
2023-05-12 15:29:09 +0100

    Add local function

--- LinkedIn/args_kwargs.py
+++ LinkedIn/args_kwargs.py
@@ -1,4 +1,5 @@
 def sum_all(*args, **kwargs):
+    print(locals())
     print(args) # tuple
     print(kwargs) # dict
     print( "SUM :", sum(args))
@@ -9,3 +10,9 @@ def sum_all(*args, **kwargs):
 sum_all(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15, name="John", age=30, city="New York")
 # (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
 # {'name': 'John', 'age': 30, 'city': 'New York'}
+
+def sum_all_with_local( *args, **kwargs):
+    print(locals())
+    # print(sum(locals()["args"]))
+
+sum_all_with_local(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15, name="John", age=30, city="New York")
