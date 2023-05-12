c73fee09758f690844aa3d4ca53306d7964fabea
Musa <79163385+musayuksel@users.noreply.github.com>
2023-05-12 13:09:26 +0100

    Add specific error message

--- PROJECT/Backend/test_main.py
+++ PROJECT/Backend/test_main.py
@@ -8,11 +8,11 @@ client = TestClient(app)
 def test_read_main(): # test the root path function named read_main
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World"}
+    assert response.json() == {"msg": "Hello World"},"SPECIFIC ERROR MESSAGE...."
 
 def test_get_current_quiz():
     response = client.get("/api/current_quiz")
     assert response.status_code == 200
-    
+
     assert response.json()["current_quiz"]["questions"][0]["question"] == "What does the acronym 'UX' stand for?"
     
