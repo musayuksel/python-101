52da7d99ebefd9b7061f385306f8273775315fa2
Musa <79163385+musayuksel@users.noreply.github.com>
2023-05-16 09:55:32 +0100

    Add modules and packages

---
+++ LinkedIn/modules_packages.py
@@ -0,0 +1,18 @@
+#import my_module # import module
+# from my_module import my_func # import function from module
+# from my_module import my_func as f # import function from module and rename it
+
+#import my_package.my_module # import module from package
+#from my_package.my_module import my_func # import function from module from package
+#file structure for package
+# my_package/
+#     __init__.py
+#     my_module.py
+#     my_other_module.py
+#     my_subpackage/
+#         __init__.py  
+#         my_submodule.py
+
+
+
+#PACKAGE FILES SHOULD HAVE AN __init__.py FILE
