d51bbe39afd1f4afbbe5e4f407e74677f39ddd17
Musa <79163385+musayuksel@users.noreply.github.com>
2023-06-01 16:36:45 +0100

    Private attributes

--- LinkedIn/python_OOP/basics.py
+++ LinkedIn/python_OOP/basics.py
@@ -9,6 +9,7 @@ class Book:
         self.author = author
         self.pages = pages
         self.price = price
+        self.__secret = "This is a secret attribute"
 
     # getters and setters for the title attribute
     def getTitle(self):
@@ -17,6 +18,15 @@ class Book:
     def setTitle(self, newTitle):
         self.title = newTitle
 
+    def set_discount(self, amount):
+        self._discount = amount  # _discount is a private variable
+
+    def get_price(self):
+        if hasattr(self, "_discount"):
+            return self.price - (self.price * self._discount)
+        else:
+            return self.price
+
 
 # TODO: create instances of the class
 book1 = Book("War and Peace", "Leo Tolstoy", 1225, 39.95)
@@ -24,3 +34,7 @@ book1 = Book("War and Peace", "Leo Tolstoy", 1225, 39.95)
 # TODO: print the class and property
 print(book1)
 print(book1.getTitle())
+book1.set_discount(0.25)
+print(book1.get_price())
+
+# print(book1.__secret)  # AttributeError: 'Book' object has no attribute '__secret'
