95c5696b74bb963b5c11920e43982a71a3ca3c56
Musa <79163385+musayuksel@users.noreply.github.com>
2023-06-01 16:57:39 +0100

    Class methods

--- LinkedIn/python_OOP/basics.py
+++ LinkedIn/python_OOP/basics.py
@@ -4,11 +4,21 @@
 class Book:
     # the "init" function is called when the instance is
     # created and ready to be initialized
-    def __init__(self, title, author, pages, price=0.0):
+    BOOK_TYPES = ("HARDCOVER", "PAPERBACK", "EBOOK")
+
+    @classmethod  # decorator to create a class method not an instance method
+    def getBookTypes(cls):
+        return cls.BOOK_TYPES
+
+    def __init__(self, title, author, pages, price=0.0, book_type="HARDCOVER"):
         self.title = title
         self.author = author
         self.pages = pages
         self.price = price
+        if book_type not in Book.BOOK_TYPES:
+            raise ValueError(f"{book_type} is not a valid book type {Book.BOOK_TYPES}")
+        else:
+            self.book_type = book_type
         self.__secret = "This is a secret attribute"
 
     # getters and setters for the title attribute
@@ -29,7 +39,8 @@ class Book:
 
 
 # TODO: create instances of the class
-book1 = Book("War and Peace", "Leo Tolstoy", 1225, 39.95)
+book1 = Book("War and Peace", "Leo Tolstoy", 1225, 39.95, "HARDCOVER")
+# book1 = Book("War and Peace", "Leo Tolstoy", 1225, 39.95, "COMIC") # ValueError: COMIC is not a valid book type ('HARDCOVER', 'PAPERBACK', 'EBOOK')
 
 # TODO: print the class and property
 print(book1)
@@ -38,3 +49,4 @@ book1.set_discount(0.25)
 print(book1.get_price())
 
 # print(book1.__secret)  # AttributeError: 'Book' object has no attribute '__secret'
+print(book1._Book__secret)  # This is a secret attribute
