f6d3b83d0a9ee889611dfb40a9a5aaf378b121d9
Musa <79163385+musayuksel@users.noreply.github.com>
2023-06-01 16:32:31 +0100

    Add getter and setter

--- LinkedIn/python_OOP/basics.py
+++ LinkedIn/python_OOP/basics.py
@@ -4,14 +4,23 @@
 class Book:
     # the "init" function is called when the instance is
     # created and ready to be initialized
-    def __init__(self, title):
+    def __init__(self, title, author, pages, price=0.0):
         self.title = title
+        self.author = author
+        self.pages = pages
+        self.price = price
+
+    # getters and setters for the title attribute
+    def getTitle(self):
+        return self.title
+
+    def setTitle(self, newTitle):
+        self.title = newTitle
 
 
 # TODO: create instances of the class
-book1 = Book("Brave New World")
-book2 = Book("War and Peace")
+book1 = Book("War and Peace", "Leo Tolstoy", 1225, 39.95)
 
 # TODO: print the class and property
 print(book1)
-print(book1.title)
+print(book1.getTitle())
