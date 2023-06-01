8e9b0194a8a331cb3a944e7b5fbe4990c9f935f2
Musa <79163385+musayuksel@users.noreply.github.com>
2023-06-01 08:37:30 +0100

    Add filter

--- LinkedIn/functional_programming/map_filter_reduce.py
+++ LinkedIn/functional_programming/map_filter_reduce.py
@@ -1,6 +1,11 @@
-number_list = range(-5, 5) # [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4]
+number_list = range(-5, 5)  # [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4]
 
 # MAP
-doubled_list = map(lambda number: number * 2, number_list) # [-10, -8, -6, -4, -2, 0, 2, 4, 6, 8]
+doubled_list = map(
+    lambda number: number * 2, number_list
+)  # [-10, -8, -6, -4, -2, 0, 2, 4, 6, 8]
 
 print(list(doubled_list))
+
+# FILTER
+positive_list = filter(lambda number: number > 0, number_list)  # [1, 2, 3, 4]
