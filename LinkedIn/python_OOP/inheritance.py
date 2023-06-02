9df807dd090edce64dcfed5832888519eb49cc2d
Musa <79163385+musayuksel@users.noreply.github.com>
2023-06-02 12:45:08 +0100

    Implement inheritance

--- LinkedIn/python_OOP/inheritance.py
+++ LinkedIn/python_OOP/inheritance.py
@@ -2,28 +2,59 @@
 # Understanding class inheritance
 
 
-class Book:
-    def __init__(self, title, author, pages, price):
+# class Book:
+#     def __init__(self, title, author, pages, price):
+#         self.title = title
+#         self.price = price
+#         self.author = author
+#         self.pages = pages
+
+
+# class Magazine:
+#     def __init__(self, title, publisher, price, period):
+#         self.title = title
+#         self.price = price
+#         self.period = period
+#         self.publisher = publisher
+
+
+# class Newspaper:
+#     def __init__(self, title, publisher, price, period):
+#         self.title = title
+#         self.price = price
+#         self.period = period
+#         self.publisher = publisher
+
+
+class Publication:
+    def __init__(self, title, price):
         self.title = title
         self.price = price
+
+
+class Book(Publication):
+    def __init__(self, title, author, pages, price):
+        super().__init__(title, price)  # call the base class constructor
         self.author = author
         self.pages = pages
 
 
-class Magazine:
+# Newspaper and Magazine are have another common base class called Periodical
+class Periodical(Publication):
     def __init__(self, title, publisher, price, period):
-        self.title = title
-        self.price = price
+        super().__init__(title, price)
         self.period = period
         self.publisher = publisher
 
 
-class Newspaper:
+class Newspaper(Periodical):
     def __init__(self, title, publisher, price, period):
-        self.title = title
-        self.price = price
-        self.period = period
-        self.publisher = publisher
+        super().__init__(title, publisher, price, period)
+
+
+class Magazine(Periodical):
+    def __init__(self, title, publisher, price, period):
+        super().__init__(title, publisher, price, period)
 
 
 b1 = Book("Brave New World", "Aldous Huxley", 311, 29.0)
