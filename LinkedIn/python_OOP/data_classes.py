6baa53e9f5f1dffd3032ecd56f7fa3801d2b14fe
Musa <79163385+musayuksel@users.noreply.github.com>
2023-06-06 11:39:27 +0100

    Repr and eq methods

--- LinkedIn/python_OOP/data_classes.py
+++ LinkedIn/python_OOP/data_classes.py
@@ -21,7 +21,16 @@ class Book:
 
 
 book1 = Book("War and Peace", "Leo Tolstoy", 1225, 39.95)
+book2 = Book("War and Peace", "Leo Tolstoy", 1225, 39.95)
 
 
 print(book1.title)  # War and Peace
 print(book1.author)  # Leo Tolstoy
+
+# it will automatically generate __init__ and __repr__ methods
+print(
+    book1
+)  # Book(title='War and Peace', author='Leo Tolstoy', pages=1225, price=39.95)
+
+# it will automatically generate __eq__ method
+print(book1 == book2)  # True
