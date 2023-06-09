e3e8b2f407a5b0b7fa11154b716be08e8534321b
Musa <79163385+musayuksel@users.noreply.github.com>
2023-06-09 12:34:28 +0100

    CRUD utils

---
+++ LinkedIn/Intermediate_projects/contact_book/crud.py
@@ -0,0 +1,36 @@
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    fake_hashed_password = user.password + "notreallyhashed"
+    db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def get_contacts(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Contact).offset(skip).limit(limit).all()
+
+
+def create_user_contact(db: Session, contact: schemas.ContactCreate, user_id: int):
+    db_contact = models.Contact(**contact.dict(), owner_id=user_id)
+    db.add(db_contact)
+    db.commit()
+    db.refresh(db_contact)
+    return db_contact
