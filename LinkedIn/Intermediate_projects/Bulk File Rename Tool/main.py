950c8c6976fab353e2dbcdd569ee4b6264cadecd
Musa <79163385+musayuksel@users.noreply.github.com>
2023-06-21 10:45:56 +0100

    filter files with extension

--- LinkedIn/Intermediate_projects/Bulk File Rename Tool/main.py
+++ LinkedIn/Intermediate_projects/Bulk File Rename Tool/main.py
@@ -6,4 +6,8 @@ prefix = input("Enter prefix (e.g holiday) :")
 
 # get a list of all files in the folder
 all_files = os.listdir(folder_path)
-print(all_files)
+
+# filter the list to only include files with the given extension
+files_with_extension = [file for file in all_files if file.endswith(extension)]
+
+print(files_with_extension)
