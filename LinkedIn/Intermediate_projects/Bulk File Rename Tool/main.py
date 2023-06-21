4fa0928905c2f0fcdbb7cdc024a5c28634917d4c
Musa <79163385+musayuksel@users.noreply.github.com>
2023-06-21 10:46:13 +0100

    Rename files

--- LinkedIn/Intermediate_projects/Bulk File Rename Tool/main.py
+++ LinkedIn/Intermediate_projects/Bulk File Rename Tool/main.py
@@ -10,4 +10,10 @@ all_files = os.listdir(folder_path)
 # filter the list to only include files with the given extension
 files_with_extension = [file for file in all_files if file.endswith(extension)]
 
-print(files_with_extension)
+# loop through the files and rename them with the given prefix
+for i, file in enumerate(files_with_extension):
+    new_name = f"{prefix}{i+1}{extension}"
+    print(f"Renaming {file} to {new_name}")
+    os.rename(os.path.join(folder_path, file), os.path.join(folder_path, new_name))
+
+print(f"All done! Renamed {len(files_with_extension)} files.")
